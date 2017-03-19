480bbaa61cff87698a6ff9f3a82be9770f00489e
Michał <zurekseventyseven@gmail.com>
2017-03-19 16:47:30 +0100

    IDE draft. Command corrections

--- Driver/manipulatorDriver/MainWindow.xaml.cs
+++ Driver/manipulatorDriver/MainWindow.xaml.cs
@@ -9,29 +9,94 @@ namespace manipulatorDriver
     public partial class MainWindow : Window
     {
         private uint it;
+        private uint speed;
         private readonly E2JManipulator manipulator;
         public MainWindow()
         {
             InitializeComponent();
             manipulator = new E2JManipulator();
             manipulator.Connect("COM3");
-            it = 1;
+            it = 6;
+            speed = 5;
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            manipulator.MoveTool(1, 20, E2JManipulator.GrabE.Open);
+            switch (it++)
+            {
+                case 1:
+                    manipulator.GrabOpen();
+                    break;
+                case 2:
+                    manipulator.Move(1);
+                    break;
+                case 3:
+                    manipulator.GrabClose();
+                    break;
+                case 4:
+                    manipulator.Draw(0,0,25);
+                    break;
+                case 5:
+                    manipulator.MoveRA(2, E2JManipulator.GrabE.Closed);
+                    break;
+                case 6:
+                    manipulator.GrabOpen();
+                    break;
+                case 7:
+                    manipulator.Draw(0,0,25);
+                    break;
+                case 8:
+                    manipulator.Move(3);
+                    break;
+                case 9:
+                    manipulator.GrabClose();
+                    break;
+                case 10:
+                    manipulator.Draw(0,0,25);
+                    break;
+                case 11:
+                    manipulator.Move(1, E2JManipulator.GrabE.Closed);
+                    break;
+                case 12:
+                    manipulator.GrabOpen();
+                    break;
+                case 13:
+                    manipulator.Draw(0,0,25);
+                    break;
+                case 14:
+                    manipulator.Move(2);
+                    break;
+                case 15:
+                    manipulator.GrabClose();
+                    break;
+                case 16:
+                    manipulator.Draw(0,0,35);
+                    break;
+                case 17:
+                    manipulator.Move(3, E2JManipulator.GrabE.Closed);
+                    break;
+                case 18:
+                    manipulator.GrabOpen();
+                    break;
+                case 19:
+                    manipulator.Draw(0, 0, 55);
+                    break;
+                default:
+                    it = 1;
+                    break;
+            }
         }
 
         private void savePosition_button_Click(object sender, RoutedEventArgs e)
         {
-            manipulator.Here(it++);
-            //manipulator.Where();
+            //manipulator.Here(3);
+            manipulator.Where();
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            manipulator.Speed(5);
+            speed = speed < 30 ? speed += 5 : 20;
+            manipulator.Speed(speed);
         }
     }
 }
---
+++ IDE/IDE/Console.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using ManipulatorDriver;
+
+namespace IDE
+{
+    public class Console : RichTextBox
+    {
+        // TODO: Text wrapper
+        // TODO: Add line counting
+        // TODO: Consider ";" as the end of instruction
+        private readonly FlowDocument document;
+        private readonly E2JManipulator manipulator;
+
+        private uint counter = 0; // Just for test
+
+        public Console()
+        {
+            document = new FlowDocument();
+            Document = document; 
+            manipulator = new E2JManipulator();
+            manipulator.Connect("COM3");
+
+            document.LineHeight = 5.0;
+        }
+
+        public void uploadProgram()
+        {
+           manipulator.LineRead(counter+=10);
+        }
+
+        public void downloadProgram()
+        {
+            var linesOfCode = new TextRange(document.ContentStart, document.ContentEnd).Text.Split('\n');
+            var lines = 0;
+            manipulator.Number("10");
+            foreach(var line in linesOfCode)
+            {
+                if (line.Length != 0)
+                {
+                    manipulator.Port.Write(line);
+                }
+            }
+            manipulator.End();
+            manipulator.Run();
+        }
+    }
+}

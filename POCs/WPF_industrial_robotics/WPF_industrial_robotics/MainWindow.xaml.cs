760b8718090a1f96277d9841dbd50f1520fec044
Adam Baniuszewicz <abaniuszewicz@gmail.com>
2017-03-20 19:24:35 +0100

    IntelliSense draft

---
+++ POCs/WPF_industrial_robotics/WPF_industrial_robotics/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.CodeCompletion;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using System.Xml;
+using System.Diagnostics;
+
+namespace WPF_industrial_robotics
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CompletionWindow completionWindow;
+        TextEditor textEditor;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeAvalon();
+            
+            
+            textEditor.TextArea.TextEntering += textEditor_TextArea_TextEntering;
+            textEditor.TextArea.TextEntered += textEditor_TextArea_TextEntered;
+        }
+
+
+        void textEditor_TextArea_TextEntered(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text == ">")
+            {
+                // Open code completion after the user has pressed > :
+                completionWindow = new CompletionWindow(textEditor.TextArea);
+                IList<ICompletionData> data = completionWindow.CompletionList.CompletionData;
+                data.Add(new MyCompletionData("GC", "opisGC"));
+                //data.Add(new MyCompletionData("GC", "opis1"));
+
+                completionWindow.Show();
+                completionWindow.Closed += delegate {
+                    completionWindow = null;
+                };
+            }
+
+            if (e.Text == "(")
+            {
+                if (textEditor.Text.Contains(","))
+                {
+                    Debug.WriteLine(textEditor.Text);
+                }
+                    
+            }
+        }
+
+        void textEditor_TextArea_TextEntering(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text.Length > 0 && completionWindow != null)
+            {
+                if (!char.IsLetterOrDigit(e.Text[0]))
+                {
+                    // Whenever a non-letter is typed while the completion window is open,
+                    // insert the currently selected element.
+                    completionWindow.CompletionList.RequestInsertion(e);
+                }
+            }
+            // Do not set e.Handled=true.
+            // We still want to insert the character that was typed.
+        }
+
+
+        private void InitializeAvalon()
+        {
+            textEditor = new TextEditor();
+            textEditor.Width = 400;
+            textEditor.Height = 200;
+            textEditor.ShowLineNumbers = true;
+            IHighlightingDefinition definition = HighlightingLoader.Load(XmlReader.Create("CustomHighlighting.xshd"), HighlightingManager.Instance);
+            HighlightingManager.Instance.RegisterHighlighting("CustomHighlighting", new[] { ".txt" }, definition);
+            textEditor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinition("CustomHighlighting");
+            panel.Children.Add(textEditor);
+        }
+    }
+}
---
+++ POCs/WPF_industrial_robotics/WPF_industrial_robotics/MyCompletionData.cs
@@ -0,0 +1,66 @@
+﻿using ICSharpCode.AvalonEdit.CodeCompletion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Editing;
+
+namespace WPF_industrial_robotics
+{
+    /// Implements AvalonEdit ICompletionData interface to provide the entries in the
+    /// completion drop down.
+    public class MyCompletionData : ICompletionData
+    {
+        string TEXT;
+        string DESCRIPTION;
+
+        public MyCompletionData(string text, string description)
+        {
+            this.TEXT = text;
+            this.DESCRIPTION = description;
+        }
+
+        public System.Windows.Media.ImageSource Image
+        {
+            get { return null; }
+        }
+
+        public string Text
+        {
+            get
+            {
+                return TEXT;
+            }
+        }
+
+        // Use this property if you want to show a fancy UIElement in the list.
+        public object Content
+        {
+            get
+            {
+                return this.Text;
+            }
+        }
+
+        public object Description
+        {
+            get
+            {
+                return DESCRIPTION;
+            }
+        }
+
+        //public double Priority => throw new NotImplementedException();
+        public double Priority => 0;
+
+        public void Complete(TextArea textArea, ISegment completionSegment,
+            EventArgs insertionRequestEventArgs)
+        {
+            textArea.Document.Replace(completionSegment, this.Text);
+        }
+    }
+}
+
+

0c598cbdba60e9db5ecf602e2cb544fc409cb6f9
Adam Baniuszewicz <abaniuszewicz@gmail.com>
2017-03-27 00:39:17 +0200

    Editor changes: * saving and loading .txt files * self-updating .txt file browser TODO: IntelliSense About changes: * updated about content

--- IDE/IDE/Views/Editor.xaml.cs
+++ IDE/IDE/Views/Editor.xaml.cs
@@ -8,6 +8,13 @@ using System.Xml;
 using System;
 using System.Windows.Input;
 using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Windows.Data;
+using System.ComponentModel;
 
 namespace IDE.Views
 {
@@ -22,13 +29,12 @@ namespace IDE.Views
         public Editor()
         {
             InitializeComponent();
+
             InitializeAvalon();
-
-            textEditor.TextArea.TextEntering += TextEntering;
-            textEditor.TextArea.TextEntered += TextEntered;
-
-            grid.Children.Add(textEditor);  
+            PopulateFileList();
         }
+
+        #region Avalon
 
         private void TextEntered(object sender, TextCompositionEventArgs e)
         {
@@ -48,6 +54,86 @@ namespace IDE.Views
             IHighlightingDefinition definition = HighlightingLoader.Load(XmlReader.Create("CustomHighlighting.xshd"), HighlightingManager.Instance);
             HighlightingManager.Instance.RegisterHighlighting("CustomHighlighting", new[] { ".txt" }, definition);
             textEditor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinition("CustomHighlighting");
+
+            Grid.SetRow(textEditor, 1);
+            Grid.SetColumn(textEditor, 1);
+            grid.Children.Add(textEditor);
+
+            textEditor.TextArea.TextEntering += TextEntering;
+            textEditor.TextArea.TextEntered += TextEntered;
         }
+
+        #endregion
+
+        #region FileViewer
+
+        private void PopulateFileList()
+        {
+            listView_FolderList.Items.Clear();
+
+            string[] files = Directory.GetFiles(@"Programs", "*.txt");
+            foreach (string file in files)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(file);
+                listView_FolderList.Items.Add(fileName);
+            }
+        }
+
+        private void listView_FolderList_click(object sender, MouseButtonEventArgs e)
+        {
+            var item = (sender as ListView).SelectedItem;
+            if (item != null)
+            {
+                textBox_ProgramName.Text = item.ToString();
+            }
+        }
+
+        #endregion
+
+        #region Save&Load
+
+        private void button_Load_Click(object sender, RoutedEventArgs e)
+        {
+            LoadSelectedItem();
+        }
+
+        private void button_Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveCurrentProgram();
+        }
+
+        private void LoadSelectedItem()
+        {
+            try
+            {
+                string selectedItem = listView_FolderList.SelectedItem.ToString();
+                textBox_ProgramName.Text = selectedItem;
+                textEditor.Text = "";
+                textEditor.Text = File.ReadAllText(@"Programs\" + selectedItem + ".txt", Encoding.ASCII);
+            }
+            catch (Exception) { };
+        }
+
+        private void SaveCurrentProgram()
+        {
+            if (textBox_ProgramName.Text != "" && textBox_ProgramName != null)
+            {
+                try
+                {
+                    File.WriteAllText(@"Programs\" + textBox_ProgramName.Text + ".txt", textEditor.Text, Encoding.ASCII);
+                    PopulateFileList();
+                }
+                catch (Exception) { };
+            }
+        }
+
+        private void TextValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^ąćęłńóśźżĄĆĘŁŃÓŚŹŻ<>:/\"|?*]");   //letters that are not allowed
+            e.Handled = !regex.IsMatch(e.Text);
+        }
+
+        #endregion
+
     }
 }
--- IDE/IDE/Views/Settings/About.xaml.cs
+++ IDE/IDE/Views/Settings/About.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace IDE.Views.Settings
 {
@@ -23,6 +12,27 @@ namespace IDE.Views.Settings
         public About()
         {
             InitializeComponent();
+            UpdateAboutText();
+        }
+
+        private void UpdateAboutText()
+        {
+            textBlock_GeneralInformations.Text = "";    //clear XAML text and update with new one
+            textBlock_GeneralInformations.Inlines.Add("This project was created as part of ");
+            textBlock_GeneralInformations.Inlines.Add(new Run("Basics of Industrial Robotics ") { FontStyle = FontStyles.Italic, FontWeight = FontWeights.Bold });
+            textBlock_GeneralInformations.Inlines.Add("subject. Our task was to create Integrated Development Environment " +
+                "that will allow to easily program Mitsubishi RV-E3J industrial robot. IDE should also have ability to browse " +
+                "through programs in manipulator's memory as well as keywords highlighting and IntelliSense-like support.");
+
+            textBlock_AboutTheCreators.Text = "";       //clear XAML text and update with new one
+            textBlock_AboutTheCreators.Inlines.Add("We are students at West Pomeranian University of Technology Szczecin, " +
+                "Faculty of Electrical Engineering. Our course of studies is Automatic Control and Robotics. Developers team " +
+                "consisted of 4 people:\n\t" +
+                "Adam Baniuszewicz\n\t" +
+                "Ewelina Chołodowicz\n\t" +
+                "Bartosz Flis\n\t" +
+                "Michał Żurawski\n" +
+                "You can contact each of us directly through the help section in top-right corner.");
         }
     }
 }

436f9f5382d4417629347cd6e024aeb69fd61049
AdBan <abaniuszewicz@gmail.com>
2017-04-06 18:22:09 +0200

    Basics of custom color highlighting (you need to add .dll from WPFtoolkit Extended)

---
+++ IDE/IDE/Common/ViewModels/HighlightingViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace IDE.Common.ViewModels
+{
+    class HighlightingViewModel : INotifyPropertyChanged
+    {
+        Button button;
+        public ObservableCollection<string> ColorCollection { get; set; }
+        public ICommand TestCommand { get; private set; }
+
+        public HighlightingViewModel()
+        {
+            ColorCollection = new ObservableCollection<string>();
+            ColorCollection.Add("Some dummy string");
+        }
+        
+
+
+        #region PropertyChangedEvents
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+    }
+}
---
+++ IDE/IDE/Common/Views/HighlightingView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using IDE.Common.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IDE.Common.Views
+{
+    /// <summary>
+    /// Interaction logic for HighlightingView.xaml
+    /// </summary>
+    public partial class HighlightingView : UserControl
+    {
+        public HighlightingView()
+        {
+            InitializeComponent();
+            DataContext = new HighlightingViewModel();
+        }
+    }
+}

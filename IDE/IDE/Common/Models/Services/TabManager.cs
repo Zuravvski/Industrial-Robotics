647d03899de181241f7b86c94c10a4276e0a81d5
Adam Baniuszewicz <abaniuszewicz@gmail.com>
2017-05-01 12:28:39 +0200

    meaningless commit

---
+++ IDE/IDE/Common/Models/Services/TabManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using Driver;
+using IDE.Common.Utilities;
+using IDE.Common.ViewModels.Commands;
+
+namespace IDE.Common.Models.Services
+{
+    public class TabManager : ObservableObject
+    {
+
+        #region Constructor
+
+        public TabManager()
+        {
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ObservableCollection<Program> Programs { get; }
+
+        #endregion
+
+        #region Actions
+
+        public void CreateProgram(string name)
+        {
+
+        }
+
+        public void LoadProgram(string path)
+        {
+
+        }
+
+        public void SaveProgram(Program program)
+        {
+
+        }
+
+        public void AddProgram(Program program)
+        {
+
+        }
+
+        public void RemoveProgram(Program program)
+        {
+
+        }
+
+        public void DeleteProgram(Program program)
+        {
+
+        }
+
+        #endregion
+    }
+}
